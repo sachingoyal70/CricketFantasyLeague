3132c0919a6832a4d0eb0d31f37bee4d6c0510f5
sachingoyal <sachingoyal@192.168.1.105>
2020-09-05 20:05:53 +0530

    Changes done to allow team modification between 8 am to 3 pm IST.

--- src/main/java/in/sachin/cricket/util/CommonConstants.java
+++ src/main/java/in/sachin/cricket/util/CommonConstants.java
@@ -14,7 +14,7 @@ public class CommonConstants {
 	public static final int TEAM_SELECTED = 1;
 
 	public static final int TEAM_APPROVED = 2;
-	
+
 	public static final int REGISTRATION_CLOSED = 3;
 
 	public static final int ACCOUNT_INACTIVE = 0;
@@ -25,8 +25,14 @@ public class CommonConstants {
 
 	public static final int TEAM_TOTAL_SUBSTITUTION = 50;
 
-	public static final String CFL_2020_START_DATE = "2020-09-19";
+	public static final String TEAM_REGISTRATION_CLOSE_DATE = "2020-09-19";
 
 	public static final String TEAM_REGISTRATION_CLOSE_TIME = "15:00:00";
+
+	public static final String TEAM_MODIFICATION_START_DATE = "2020-09-26";
+
+	public static final String TEAM_MODIFICATION_START_TIME = "08:00:00";
+
+	public static final String TEAM_MODIFICATION_END_TIME = "15:00:00";
 
 }
